fc119b3a985770b67da8e87dbe7ec5e8db7c808f
radio <radio.github@gmail.com>
2013-05-25 19:15:08 -0700

    Scaladoc for Genre object

--- src/main/scala/com/github/hateradio/genre/Genre.scala
+++ src/main/scala/com/github/hateradio/genre/Genre.scala
@@ -2,22 +2,43 @@ package com.github.hateradio.GenreFormat
 
 import org.apache.commons.lang3.text.WordUtils
 
+/** Genre normalizer
+ * 
+ * This is the heart of the application. Maintains common fixes and the format method.
+ */
 object Genre {
 
+  /** Map of common corrections */
   private val corrections = Map(
     "acappella" -> "A Capella",
     "acapella" -> "A Capella",
     "acapela" -> "A Capella",
     "avantgarde" -> "Avant-Garde",
-    "avant garde" -> "Avant-Garde",
     "edm" -> "EDM",
+    "electro" -> "Electronic",
     "electronica" -> "Electronic",
+    "folkrock" -> "Folk-Rock",
     "idm" -> "IDM",
     "postrock" -> "Post-Rock"
   )
 
-  private def fix(s: String): String = corrections.getOrElse(s.replaceAll("\\s+", "").toLowerCase, s)
+  /** Fixes a string if matched in the corrections map
+   *
+   * Replaces white space and converts a string to lower case to find a match
+   *
+   * @param genre the word to fix
+   * @return a correct genre
+   */
+  private def fix(genre: String): String = corrections.getOrElse(genre.replaceAll("\\s+", "").toLowerCase, genre)
 
+  /** Formats a string of genres
+   *
+   * Strips and replaces unnecessary text, converts "and" to "&", capitalizes and fixes words.
+   *
+   * @param s the string of genres to format
+   * @return a formatted string of unique genres separated by semi-colons
+   *
+   */
   def format(s: String): String = {
     s.replaceAll("Genre ?:? ?(?:.+?multiple values.+? )?", "")
       .replaceAll("[<>?|:_.\"]", " ")
